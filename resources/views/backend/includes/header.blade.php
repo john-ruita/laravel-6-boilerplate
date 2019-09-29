ec60fca7dd0ca8663eba554633b24a3d54445cef
John Ruita <w.johnruita@gmail.com>
2019-09-29 11:51:22 +0300

    card-tools

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -35,7 +35,7 @@
         <li class="nav-item dropdown">
             <a class="nav-link" data-toggle="dropdown" href="#">
                 <i class="far fa-bell"></i>
-                <span class="badge badge-warning navbar-badge">0</span>
+                <span class="badge badge-info navbar-badge">0</span>
             </a>
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                 <span class="dropdown-header">0 Notifications</span>
